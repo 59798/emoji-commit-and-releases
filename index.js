c32caf0d54a16f356731c4e3ba84d79675c971d8
59naga <i59naga@icloud.com>
2016-06-07 13:59:15 +0900

    :shirt: linterの警告を全て修正

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
- module.exports = ()=>{
-   return 1
- }
+ module.exports = () => {
+   return 1;
+ };
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const EmojiCommitAndReleases = require('./')
-const assert = require('assert')
+const assert = require('assert');
+const emojiCommitAndReleases = require('./');
 
-it('EmojiCommitAndReleasesは1を返すべき', ()=>{
-  assert(EmojiCommitAndReleases() === 1);
-})
+it('emojiCommitAndReleasesは1を返すべき', () => {
+  assert(emojiCommitAndReleases() === 1);
+});
